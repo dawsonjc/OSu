8867210f8a9c75a6d8e6b2f349a97f1fb95a001b
dawsonjc <thedeadgamer7777@gmail.com>
2022-06-22 22:10:42 -0700

    bruh

--- kernel/src/Kernel.cpp
+++ kernel/src/Kernel.cpp
@@ -1,11 +1,17 @@
 #include <kernelUtil.h>
 
+int testFunction(bool withValue, bool hasValue);
+
 extern "C" void _start(BootInfo *bootInfo)
 {
     KernelInfo kernelInfo = InitializeKernel(bootInfo);
     PageTableManager* pageTableManager = kernelInfo.pageTableManager;
 
     GlobalRenderer.PrintLine("Kernel Initialized Successfully", 0xFF00FF00);
+
+    int testValue = testFunction(true, true);
+
+    GlobalRenderer.PrintLine("Test Vale init: ", testValue);
 
     while (true) {
         asm("hlt");
